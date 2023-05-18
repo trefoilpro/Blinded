282511e7d41d2fd56ea9af11289ad69ccbcbee34
noshuk <noshuk01@gmail.com>
2023-05-18 04:34:12 +0300

    pusg

--- Blinded/Assets/Scripts/PlayerScripts/Blind.cs
+++ Blinded/Assets/Scripts/PlayerScripts/Blind.cs
@@ -6,15 +6,27 @@ using UnityEngine.UI;
 public class Blind : MonoBehaviour
 {
     [SerializeField] private Image _blindnessImage;
-
+    [SerializeField] private Image _blindnessImage2;
+    [SerializeField] private int _waitSeconds;
+    [Range(0,1)] [SerializeField] private float _worsenBy;
+    private Color currentColor;
+    private Color currentColor2;
     private void Start()
     {
-        _blindnessImage.color = new Color(0, 0, 0, 0);
+        currentColor = new Color(0, 0, 0, 0);
+        currentColor2 = new Color(0, 0, 0, 0);
+        _blindnessImage.color = currentColor;
+        _blindnessImage2.color = currentColor2;
+        StartCoroutine(WorsenVision());
     }
 
     IEnumerator WorsenVision()
     {
-
-        yield return WaitForSeconds();
+        yield return new WaitForSeconds(_waitSeconds);
+        currentColor += new Color(0, 0, 0, _worsenBy);
+        currentColor2 += new Color(0, 0, 0, _worsenBy * .8f);
+        _blindnessImage.color = currentColor;
+        _blindnessImage2.color = currentColor2;
+        StartCoroutine(WorsenVision());
     }
 }
